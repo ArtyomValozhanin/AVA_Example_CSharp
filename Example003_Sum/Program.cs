7f131cb3dfd626c6750b2a1aae823b0f065fc723
AVA <Marsota_mixer@tut.by>
2023-04-06 00:51:16 +0300

    modify Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 7;
-int numberB = 15;
+﻿int numberA = new Random().Next (1,11); // случайное число от 1 до 10 !
+Console.Write ("Number A=");
+Console.WriteLine (numberA);
+int numberB = new Random().Next (1,21); // случайное число от 1 до 20 !
+Console.Write ("Number B=");
+Console.WriteLine (numberB);
 int result = numberA + numberB;
-Console.Write ("Result=");
+Console.Write ("Result A+B=");
 Console.Write (result);
