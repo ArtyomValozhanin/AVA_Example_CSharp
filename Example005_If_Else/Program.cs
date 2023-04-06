f6540fe16a8260f170d5cf1cbcc4494129cde50d
AVA <Marsota_mixer@tut.by>
2023-04-06 10:46:38 +0300

    add Example005_If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Enter user name: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "vasya")
+{
+    Console.WriteLine ("Oh no, it`s VASYA!");
+}
+else
+{
+Console.Write ("Hi, ");  
+Console.WriteLine (username);
+} 
+
