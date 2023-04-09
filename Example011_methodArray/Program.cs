63e6dd85059276f89679ccf2e5134ae4a2c0d512
AVA <Marsota_mixer@tut.by>
2023-04-09 13:38:56 +0300

    added Example011

---
+++ Example011_methodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// найти первый элемент соответвующий значению "find" 
+// и показать его index в массиве
+
+int[] array = { 10, 21, 43, 4, 52, 16, 75, 38, 52 };
+
+int n = array.Length; // Length - возвращает длину элементов массива
+int find = 52;
+
+int index = 0;
+
+while (index < n)
+{
+        if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break
+    }
+    index++; // index = Index + 1
+}
