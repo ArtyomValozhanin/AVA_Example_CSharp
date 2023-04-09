5d437de7a99b57ba87badcc59a0ab876992bc201
AVA <Marsota_mixer@tut.by>
2023-04-09 12:24:04 +0300

    work whith Example008

--- Example008_2Friend1Dog/Program.cs
+++ Example008_2Friend1Dog/Program.cs
@@ -1,24 +1,29 @@
-﻿int count = 0;
+﻿int FFS = 1;
+Console.WriteLine ($"First friend speed: {FFS}");
+int SFS = 2;
+Console.WriteLine ($"Second friend speed: {SFS}");
+int DS = 5;
+Console.WriteLine ($"Dog speed {DS}");
+int Time = 0;
+int Friend = 1;
 int distance = 1000;
-int FirstFriendSpeed = 1;
-int SecondFriendSpeed = 2;
-int DogSpeed =5;
+int count = 0;
 
 while (distance > 10)
 {
-    if (FirstFriendSpeed == 1);
+    if (FFS == 1)
     {
-        Time = distance / (FirstFriendSpeed + DogSpeed);
+        Time = distance / (FFS + DS);
         Friend = 2;
-    
+    }
 
     else
-        
-        Time = distance / (SecondFriendSpeed + DogSpeed);
+    {    
+        Time = distance / (SFS + DS);
         Friend = 1;
     }
 
-    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * Time;
+    distance = distance - (FFS + SFS) * Time;
     Console.WriteLine(count);
     count++;
 }
